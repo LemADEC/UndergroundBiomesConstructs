cb04d1d6d265311d172147aca394f7d9641bea46
LemADEC <lemadec.fr@gmail.com>
2016-02-21 22:58:46 +0100

    Fixed missing hashcode override
    Code cleanup

--- src/main/java/exterminatorJeff/undergroundBiomes/api/BlockCodes.java
+++ src/main/java/exterminatorJeff/undergroundBiomes/api/BlockCodes.java
@@ -9,43 +9,46 @@ import Zeno410Utils.*;
 import net.minecraft.block.Block;
 
 public class BlockCodes extends BlockState {
-    public final NamedBlock name;
-    public final BlockCodes onDrop;
-    private final int metadataHashcode;
-
-    public BlockCodes(Block block, int metadata) {
-        super(block,metadata);
-        name = null;
-        onDrop = this;
-        metadataHashcode = new Integer(metadata).hashCode();
-    }
-    
-    public BlockCodes(NamedBlock namer, int metadata) {
-        super(namer.block(),metadata);
-        name = namer;
-        if (block == null) {
-            throw new RuntimeException("couldn't find block for "+namer.internal());
-        }
-        onDrop = this;
-        metadataHashcode = new Integer(metadata).hashCode();
-    }
-
-    public BlockCodes(NamedBlock namer, int metadata, BlockCodes onDrop) {
-        super(namer.block(),metadata);
-        name = namer;
-        this.onDrop = onDrop;
-        metadataHashcode = new Integer(metadata).hashCode();
-    }
-
-    public int hashcode() {
-        return block.hashCode()+metadataHashcode;
-    }
-
-    public boolean equals(Object compared) {
-        if (compared instanceof BlockCodes) {
-            BlockCodes comparedCodes = (BlockCodes)compared;
-            if ((block==comparedCodes.block)&&(metadata == comparedCodes.metadata)) return true;
-        }
-        return false;
-    }
+	public final NamedBlock name;
+	public final BlockCodes onDrop;
+	private final int metadataHashCode;
+	
+	public BlockCodes(Block block, int metadata) {
+		super(block, metadata);
+		name = null;
+		onDrop = this;
+		metadataHashCode = new Integer(metadata).hashCode();
+	}
+	
+	public BlockCodes(NamedBlock namer, int metadata) {
+		super(namer.block(), metadata);
+		name = namer;
+		if (block == null) {
+			throw new RuntimeException("couldn't find block for " + namer.internal());
+		}
+		onDrop = this;
+		metadataHashCode = new Integer(metadata).hashCode();
+	}
+	
+	public BlockCodes(NamedBlock namer, int metadata, BlockCodes onDrop) {
+		super(namer.block(), metadata);
+		name = namer;
+		this.onDrop = onDrop;
+		metadataHashCode = new Integer(metadata).hashCode();
+	}
+	
+	@Override
+	public int hashCode() {
+		return block.hashCode() + metadataHashCode;
+	}
+	
+	@Override
+	public boolean equals(Object compared) {
+		if (compared instanceof BlockCodes) {
+			BlockCodes comparedCodes = (BlockCodes) compared;
+			if ((block == comparedCodes.block) && (metadata == comparedCodes.metadata))
+				return true;
+		}
+		return false;
+	}
 }
