dff53b3652f5bbb0e8a0cc434452e169a9989037
LemADEC <lemadec.fr@gmail.com>
2016-02-29 00:38:37 +0100

    Fixed command message

--- src/main/java/exterminatorJeff/undergroundBiomes/common/command/CommandOreDictifyStone.java
+++ src/main/java/exterminatorJeff/undergroundBiomes/common/command/CommandOreDictifyStone.java
@@ -2,40 +2,39 @@ package exterminatorJeff.undergroundBiomes.common.command;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import exterminatorJeff.undergroundBiomes.common.UndergroundBiomes;
 
-public class CommandOreDictifyStone extends CommandBase
-{
-
-    public CommandOreDictifyStone(){}
-
-    public int compareTo(Object compared) {
-        return this.toString().compareTo(compared.toString());
-    }
-
-
-    public String getCommandName(){
-        return "oredictifystone";
-    }
-
-    public String getCommandUsage(ICommandSender par1ICommandSender){
-        return "/" + this.getCommandName();
-    }
-
-    public void processCommand(ICommandSender sender, String as[])
-    {
-        try
-        {
-            int num = UndergroundBiomes.oreDictifyStone();
-            //sender.func_110122_a(ChatMessageComponent.func_111077_e(String.format("commands.oredictifystone.ok", num)).func_111059_a(EnumChatFormatting.GREEN));
-            //sender.sendChatToPlayer(String.format("\u00a7aStone OreDictify complete, modified %d recipes.", num));
-        }
-        catch (Exception e)
-        {
-            //sender.func_110122_a(ChatMessageComponent.func_111077_e("commands.oredictifystone.fail").func_111059_a(EnumChatFormatting.RED));
-            //sender.sendChatToPlayer("\u00a7cStone OreDictify has failed!");
-            e.printStackTrace();
-        }
-    }
+public class CommandOreDictifyStone extends CommandBase {
+	
+	public CommandOreDictifyStone() {
+	}
+	
+	@Override
+	public int compareTo(Object compared) {
+		return this.toString().compareTo(compared.toString());
+	}
+	
+	@Override
+	public String getCommandName() {
+		return "oredictifystone";
+	}
+	
+	@Override
+	public String getCommandUsage(ICommandSender par1ICommandSender) {
+		return "/" + this.getCommandName();
+	}
+	
+	@Override
+	public void processCommand(ICommandSender sender, String as[]) {
+		try {
+			int num = UndergroundBiomes.oreDictifyStone();
+			sender.addChatMessage(new ChatComponentText("" + EnumChatFormatting.GREEN).appendText(StatCollector.translateToLocalFormatted("commands.oredictifystone.ok", num)));
+		} catch (Exception exception) {
+			sender.addChatMessage(new ChatComponentText("" + EnumChatFormatting.GREEN).appendText(StatCollector.translateToLocalFormatted("commands.oredictifystone.fail")));
+			exception.printStackTrace();
+		}
+	}
 }
